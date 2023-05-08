250abddfc4b909fe00e73d3d8734e29384158950
John Vinh <john.michael.vinh2@gmail.com>
2023-05-08 10:59:47 -0400

    Make code shorter and more pythonic

--- top_k_frequent_elements.py
+++ top_k_frequent_elements.py
@@ -5,9 +5,4 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
-        output = []
-
-        for number in counter.most_common(k):
-            output.append(number[0])
-
-        return output
+        return [number[0] for number in counter.most_common(k)]
