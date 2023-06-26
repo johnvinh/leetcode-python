824a939b7355e52aa84cfc3ef9e1430b117f392e
John Vinh <john.michael.vinh2@gmail.com>
2023-06-25 22:05:13 -0400

    Solve remove element

--- remove_element.py
+++ remove_element.py
@@ -3,4 +3,9 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        pass
+        different = []
+        for num in nums:
+            if num != val:
+                different.append(num)
+        nums[:len(different)] = different
+        return len(different)
