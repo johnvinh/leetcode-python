195c62a65df45eec7fbe3496aa1c0665400c3c09
John Vinh <john.michael.vinh2@gmail.com>
2023-06-25 22:37:58 -0400

    Solve plus one using the silly method

--- plus_one_easy.py
+++ plus_one_easy.py
@@ -1,2 +1,7 @@
+from typing import List
+
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # lol
+        return [int(num) for num in str(int(''.join([str(digit) for digit in digits])) + 1)]
